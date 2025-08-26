2c618b82e67af0ab21d2016a8172de2ed0bd5ae2
zack1stplayer <zack1stplayer@gmail.com>
2025-08-25 19:42:46 -0700

    Moving function definitions from the .hpp to the .cpp

--- Student.cpp
+++ Student.cpp
@@ -1,1 +1,31 @@
 #include "Student.hpp"
+
+Student::Student()
+{
+    this->name = new string();
+    this->age = new int();
+    cout << "Student object created" << endl;
+}
+
+Student::~Student()
+{
+    delete this->name;
+    delete this->age;
+    cout << "Student object destroyed" << endl;
+}
+
+void Student::setName(string var) {
+    *this->name = var;
+}
+
+string Student::getName() const {
+    return *this->name;
+}
+
+void Student::setAge(int var) {
+    *this->age = var;
+}
+
+int Student::getAge() const {
+    return *this->age;
+}
--- Student.hpp
+++ Student.hpp
@@ -18,33 +18,3 @@ public:
     void setAge(int var);
     int getAge() const;
 };
-
-Student::Student()
-{
-    this->name = new string();
-    this->age = new int();
-    cout << "Student object created" << endl;
-}
-
-Student::~Student()
-{
-    delete this->name;
-    delete this->age;
-    cout << "Student object destroyed" << endl;
-}
-
-void Student::setName(string var) {
-    *this->name = var;
-}
-
-string Student::getName() const {
-    return *this->name;
-}
-
-void Student::setAge(int var) {
-    *this->age = var;
-}
-
-int Student::getAge() const {
-    return *this->age;
-}
